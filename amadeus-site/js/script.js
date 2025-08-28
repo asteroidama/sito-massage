5115a7b2dc39459e81181cf1dcdd77ad9466d277
asteroidama <amadmazza@gmail.com>
2025-08-28 23:38:17 +0200

    Delete amadeus-site directory

--- amadeus-site/js/script.js
+++
@@ -1,9 +0,0 @@
-// Seleziono bottone e menu
-const navToggle = document.querySelector(".nav-toggle");
-const navLinks = document.querySelector(".nav-links");
-
-// Aggiungo evento di click
-navToggle.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  navToggle.classList.toggle("active");
-});
